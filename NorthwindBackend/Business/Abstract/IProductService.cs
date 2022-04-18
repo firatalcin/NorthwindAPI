a48471dc4eb600e6f0cfd9bb58e8ced9f480179d
Fırat Alçın <firatalcin04@gmail.com>
2022-04-18 22:44:23 +0300

    Update

--- NorthwindBackend/Business/Abstract/IProductService.cs
+++ NorthwindBackend/Business/Abstract/IProductService.cs
@@ -1,4 +1,5 @@
 ﻿using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace Business.Abstract
         Product GetById(int id);
         List<Product> GetAllByCategoryId(int categoryId);
         List<Product> GetByUnitPrice(decimal min, decimal max);
+        List<ProductDetailDto> GetProductDetails();
         void Add(Product product);
         void Update(Product product);
         void Delete(Product product);
--- NorthwindBackend/ConsoleUI/Program.cs
+++ NorthwindBackend/ConsoleUI/Program.cs
@@ -8,14 +8,37 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+
+            //CategoryTest();
+
             ProductManager productManager = new ProductManager(new EfProductDal());
-            
+
+            foreach (var item in productManager.GetProductDetails())
+            {
+                Console.WriteLine(item.ProductName + " " + item.CategoryName);
+            }
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var item in categoryManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
             foreach (var item in productManager.GetAll())
             {
                 Console.WriteLine(item.ProductName + " " + item.UnitsInStock);
             }
-
-
         }
     }
 }
