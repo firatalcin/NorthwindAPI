6e8ca59a4388df0a9f38dc214efa0478e13c0ca3
Fırat Alçın <firatalcin04@gmail.com>
2022-04-18 17:02:21 +0300

    Update

---
+++ NorthwindBackend/Core/DataAccess/Concrete/EntityFramework/EfEntityRepository.cs
@@ -0,0 +1,46 @@
+﻿using Core.DataAccess.Abstract;
+using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.Concrete.EntityFramework
+{
+    public class EfEntityRepository<TEntity, TContext> : IEntityRepository<TEntity> 
+        where TEntity : class, IEntity, new()
+        where TContext : DbContext, new()
+    {
+        public void Add(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
