d8caafb30f463bac1d803faf719e1a318ec60c3f
Fırat Alçın <firatalcin04@gmail.com>
2022-04-18 19:02:04 +0300

    Update

--- NorthwindBackend/Core/DataAccess/Concrete/EntityFramework/EfEntityRepository.cs
+++ NorthwindBackend/Core/DataAccess/Concrete/EntityFramework/EfEntityRepository.cs
@@ -25,22 +25,38 @@ namespace Core.DataAccess.Concrete.EntityFramework
 
         public void Delete(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
         }
 
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                return context.Set<TEntity>().Where(filter).SingleOrDefault();
+            }
         }
 
         public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                return filter == null ? 
+                    context.Set<TEntity>().ToList() :
+                    context.Set<TEntity>().Where(filter).ToList();
+            }
         }
 
         public void Update(TEntity entity)
         {
-            throw new NotImplementedException();
+            using (TContext context = new TContext())
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
         }
     }
 }
--- NorthwindBackend/DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ NorthwindBackend/DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.Concrete.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -10,51 +11,8 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : IProductDal
+    public class EfProductDal : EfEntityRepository<Product, NorthwindContext>, IProductDal
     {
-        public void Add(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                context.Add(entity);
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                context.Remove(entity);
-                context.SaveChanges();
-            }
-        }
-
-        public Product Get(Expression<Func<Product, bool>> filter)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return context.Set<Product>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                return filter == null ?
-                    context.Set<Product>().ToList() :
-                    context.Set<Product>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Product entity)
-        {
-            using (NorthwindContext context = new NorthwindContext())
-            {
-                context.Update(entity);
-                context.SaveChanges();
-            }
-        }
+       
     }
 }
