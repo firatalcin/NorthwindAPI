09d2e247aedb4377e5ed20e138cf87bcad9c2c32
Fırat Alçın <firatalcin04@gmail.com>
2022-04-20 17:16:38 +0300

    Update

---
+++ NorthwindBackend/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public void Get()
+        {
+           
+            var result = _productService.GetAll();
+            if (result.Success == true)
+            {
+                 Ok(result.Data);
+            }
+            else
+            {
+                BadRequest(result.Data);
+            }
+        }
+    }
+}
