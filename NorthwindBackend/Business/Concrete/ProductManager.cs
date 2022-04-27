a88891ad71483dccc25a89326a8f17e7f984d0c1
Fırat Alçın <firatalcin04@gmail.com>
2022-04-27 17:24:32 +0300

    Update

--- NorthwindBackend/Business/Concrete/ProductManager.cs
+++ NorthwindBackend/Business/Concrete/ProductManager.cs
@@ -80,6 +80,7 @@ namespace Business.Concrete
             return new ErrorResult(Messages.ProductUpdatedInvalid);
         }
 
+        //[CacheAspect] key,value
         public IDataResult<List<Product>> GetAll()
         {
             if (DateTime.Now.Hour == 24)
